7527d0e85dfe1853fe877bce346b060592d5d63c
Marion Thaureau <142398963+Marion-Thaureau@users.noreply.github.com>
2023-10-21 19:47:42 +0200

    Update assessing_models_shift_OU_adequacy.R

--- assessing_models/assessing_models_shift_OU_adequacy.R
+++ assessing_models/assessing_models_shift_OU_adequacy.R
@@ -48,7 +48,7 @@ if (length(OU_mov_opt) > 0) {
 
 if (length(Stasis_OU_subset2) > 0) {
   for (i in 1:length(Stasis_OU_subset2)) {
-   Stasis_OU_subset2_adeq[[i]] <- fit3adequacy.OU(Stasis_OU_subset2[[i]], plot = FALSE) #not working for the timeseries 2 (number 75)
+   Stasis_OU_subset2_adeq[[i]] <- fit3adequacy.OU(Stasis_OU_subset2[[i]], plot = FALSE)
   }
 }
 
@@ -98,26 +98,3 @@ if (length(OU_Stasis_subset1) > 0) {
 save(OU_adeq, OU_mov_opt_anc_adeq, OU_mov_opt_adeq, Stasis_OU_subset2_adeq, 
            URW_OU_subset2_adeq, GRW_OU_subset2_adeq, OU_OU_subset1_adeq, OU_OU_subset2_adeq, 
            OU_GRW_subset1_adeq, OU_URW_subset1_adeq, OU_Stasis_subset1_adeq, file="OU_subset_adeq.Rdata")
-
-
-
-#remove problematic timeseries 
-
-Stasis_OU_subset2_adeqn = list()
-j = c(1,3,4,5,6,7,8,9)
-for (i in 1:length(j)) {
-k = j[i]
-Stasis_OU_subset2_adeqn[[i]] = Stasis_OU_subset2_adeq[[k]]
-}
-
-
-Stasis_OU_subset1_new = list()
-j = c(1,3,4,5,6,7,8,9)
-for (i in 1:length(j)) {
-  k = j[i]
-  Stasis_OU_subset1_new[[i]] = Stasis_OU_subset1[[k]]
-}
-
-Stasis_OU_subset2_adeq = Stasis_OU_subset2_adeqn
-Stasis_OU_subset1 = Stasis_OU_subset1_new
-
