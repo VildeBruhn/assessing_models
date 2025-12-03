0223b6fea9c7181c0a943f09eb7c92267c2b4ad8
Marion Thaureau <142398963+Marion-Thaureau@users.noreply.github.com>
2025-12-03 14:40:19 +0100

    update of the results of statistics

--- assessing_models/statistics_uni.R
+++ assessing_models/statistics_uni.R
@@ -1,6 +1,6 @@
 #########################################
 ##   Assessing models of evolution     ##
-##          STATISTICS                 ##
+##            STATISTICS               ##
 #########################################
 
 # install packages
@@ -16,6 +16,10 @@ library(tidyverse)
 library(devtools)
 library(wesanderson)
 library(gridExtra)
+library(ggplot2)
+library(lme4)
+library(lmerTest)
+library(dplyr)
 
 rm(list = ls())
 
@@ -68,7 +72,7 @@ ln_data_meta <- model_adeq(ln_data_meta, OU_mov_opt_anc_adeq_passed, "OU mov opt
 ln_data_meta <- model_adeq(ln_data_meta, OU_mov_opt_adeq_passed, "OU mov opt")
 
 # bind data to dataframe
-unit_list <- c("total_N", "steps", "interval_MY", "trait_type", "microfossil",
+unit_list <- c("popID", "total_N", "steps", "interval_MY", "trait_type", "microfossil",
                "lat", "lon", "sediment", "model_aicc", "model_adequate", 
                "environment")
 plot_data <- bind(ln_data_meta, unit_list)
@@ -82,9 +86,9 @@ plot_data$model_aicc <- replace(plot_data$model_aicc, plot_data$model_aicc == "O
 plot_data$model_adequate <- replace(plot_data$model_adequate, plot_data$model_adequate == "OU mov opt anc", "OU mov opt")
 
 # collapse all marine into one
-plot_data$environment <- replace(plot_data$environment, plot_data$environment == "marine benthic", "marine")
-plot_data$environment <- replace(plot_data$environment, plot_data$environment == "marine pelagic", "marine")
-plot_data$environment <- replace(plot_data$environment, plot_data$environment == "marine planktic", "marine")
+#plot_data$environment <- replace(plot_data$environment, plot_data$environment == "marine benthic", "marine")
+#plot_data$environment <- replace(plot_data$environment, plot_data$environment == "marine pelagic", "marine")
+#plot_data$environment <- replace(plot_data$environment, plot_data$environment == "marine planktic", "marine")
 
 # add parameters column
 plot_data$parameters <- plot_data$model_aicc
@@ -98,7 +102,12 @@ plot_data$parameters <- replace(plot_data$parameters, plot_data$parameters == "O
 
 # remove time series with NA
 plot_data <- plot_data %>% drop_na(model_aicc)
-plot_data <- plot_data %>% drop_na(environment)
+#plot_data <- plot_data %>% drop_na(environment)
+
+# ordering the model according to the number of parameters
+level_order <- c("stasis", "URW", "GRW", "accel", "decel", "OU", "OU mov opt")
+plot_data$model_aicc <- factor(plot_data$model_aicc, levels = level_order)
+plot_data$model_aicc <- relevel(plot_data$model_aicc, ref = "stasis")
 
 # set colors
 col_val1 <- c("#85B7B9", "#DCCB4E")
@@ -110,36 +119,36 @@ col_val_extra <- c("#3A9AB2", "#85B7B9", "#ADC397", "#DCCB4E", "#E5A208", "#ED6E
 # PLOT RELATIVE FIT
 #-------------------
 
+###### micro vs. macro ######
+#micro_macro <- plot_data
+#level_order <- c("stasis", "URW", "GRW", "accel", "decel", "OU", "OU mov opt")
+#pdf("./results_paleoTS_v0.6.1/plot/micro_macro_aicc.pdf")
+#ggplot(micro_macro, aes(x = factor(model_aicc, levels = level_order), fill = microfossil)) + geom_bar() +
+#  theme_classic() + scale_x_discrete(labels = c("Stasis", "URW", "GRW", "Accel.", "Decel.", "OU", "OU mov. opt.")) +
+#  labs(fill = "") + scale_fill_discrete(name = "", labels = c("Macrofossils", "Microfossils"), palette = col_val1) +
+#  xlab("Model") + ylab("Count") + theme(legend.text = element_text(size = 15))
+#dev.off()
 
-# micro vs. macro
-micro_macro <- plot_data
-level_order <- c("stasis", "URW", "GRW", "accel", "decel", "OU", "OU mov opt")
-pdf("./results_paleoTS_v0.6.1/plot/micro_macro_aicc.pdf")
-ggplot(micro_macro, aes(x = factor(model_aicc, levels = level_order), fill = microfossil)) + geom_bar() +
-  theme_classic() + scale_x_discrete(labels = c("Stasis", "URW", "GRW", "Accel.", "Decel.", "OU", "OU mov. opt.")) +
-  labs(fill = "") + scale_fill_discrete(name = "", labels = c("Macrofossils", "Microfossils"), palette = col_val1) +
-  xlab("Model") + ylab("Count") + theme(legend.text = element_text(size = 15))
-dev.off()
+###### environment ######
+#env <- plot_data
+#level_order <- c("stasis", "URW", "GRW", "accel", "decel", "OU", "OU mov opt")
+#pdf("./results_paleoTS_v0.6.1/plot/environment_aicc.pdf")
+#ggplot(env, aes(x = factor(model_aicc, levels = level_order), fill = environment)) + geom_bar() +
+#  theme_classic() + 
+#  scale_x_discrete(labels = c("Stasis", "URW", "GRW", "Accel.", "Decel.", "OU", "OU mov. opt.")) +
+#  labs(fill = "") + scale_fill_discrete(name = "Environment", labels = c("Fluvial", "Lacustrine", "Marine",
+#                                                                         "Terrestrial"), palette = col_val2) +
+#  xlab("Model") + ylab("Count") + theme(legend.text = element_text(size = 10), 
+#                                        legend.title = element_text(size = 12),
+#                                        axis.title = element_text(size = 12))
+#dev.off()
 
-# environment
-env <- plot_data
-level_order <- c("stasis", "URW", "GRW", "accel", "decel", "OU", "OU mov opt")
-pdf("./results_paleoTS_v0.6.1/plot/environment_aicc.pdf")
-ggplot(env, aes(x = factor(model_aicc, levels = level_order), fill = environment)) + geom_bar() +
-  theme_classic() + 
-  scale_x_discrete(labels = c("Stasis", "URW", "GRW", "Accel.", "Decel.", "OU", "OU mov. opt.")) +
-  labs(fill = "") + scale_fill_discrete(name = "Environment", labels = c("Fluvial", "Lacustrine", "Marine",
-                                                                         "Terrestrial"), palette = col_val2) +
-  xlab("Model") + ylab("Count") + theme(legend.text = element_text(size = 10), 
-                                        legend.title = element_text(size = 12),
-                                        axis.title = element_text(size = 12))
-dev.off()
 
-# interval MY
+###### interval MY ######
 intv_my <- plot_data[c("model_aicc", "interval_MY", "parameters")]
 
 
-# stats
+# stats 
 intv_my_mean <- aggregate(intv_my[, 2], list(intv_my$model_aicc), mean)
 intv_my_mean <- as.data.frame(intv_my_mean)
 names(intv_my_mean) <- c("model", "mean interval (MY)")
@@ -151,6 +160,11 @@ sink(file = "./results_paleoTS_v0.6.1/interval_my_results_aicc.txt")
 intv_my_mean
 intv_my_median
 sink()
+
+# LMM
+#lmm_model_time <- lmer(interval_MY ~ 1+ model_aicc + (1| popID), data = plot_data)
+lmm_model_time <- lm(interval_MY ~ model_aicc, data = plot_data)
+summary(lmm_model_time)
 
 # plot
 level_order <- c("stasis", "URW", "GRW", "accel", "decel", "OU", "OU mov opt")
@@ -168,7 +182,7 @@ intv_plot <- ggplot(intv_my, aes(x = interval_MY, y = factor(model_aicc, levels 
 intv_plot
 dev.off()
 
-# steps
+###### steps ######
 steps <- plot_data[c("model_aicc", "steps", "parameters")]
 
 # stats
@@ -183,6 +197,11 @@ sink(file = "./results_paleoTS_v0.6.1/steps_uni_results_aicc.txt")
 steps_mean
 steps_median
 sink()
+
+# LMM
+#lmm_model_steps <- lmer(steps ~ model_aicc + (1| popID), data = plot_data)
+lmm_model_steps <- lm(steps ~ model_aicc, data = plot_data)
+summary(lmm_model_steps)
 
 # plot
 level_order <- c("stasis", "URW", "GRW", "accel", "decel", "OU", "OU mov opt")
@@ -199,7 +218,7 @@ steps_plot <- ggplot(steps, aes(x = log(steps), y = factor(model_aicc, levels = 
 steps_plot
 dev.off()
 
-# resolution
+###### resolution ######
 res <- plot_data[c("model_aicc", "steps", "interval_MY", "parameters")]
 res$resolution <- res$steps/res$interval_MY
 
@@ -215,6 +234,12 @@ sink(file = "./results_paleoTS_v0.6.1/resolution_uni_results_aicc.txt")
 res_mean
 res_median
 sink()
+
+# LMM
+plot_data$resolution = plot_data$steps/plot_data$interval_MY
+#lmm_model_resolution <- lmer(resolution ~ 1+ model_aicc + (1| popID), data = plot_data)
+lmm_model_resolution <- lm(resolution ~ model_aicc, data = plot_data)
+summary(lmm_model_resolution)
 
 # plot
 level_order <- c("stasis", "URW", "GRW", "accel", "decel", "OU", "OU mov opt")
@@ -241,6 +266,28 @@ grid.arrange(intv_plot,
 dev.off()
 
 
+
+# put the output of the linear mixed effects models in the same table
+models = rownames(summary(lmm_model_time)$coefficients)
+interval = as.data.frame(summary(lmm_model_time)$coefficients)
+steps = as.data.frame(summary(lmm_model_steps)$coefficients)
+resolution = as.data.frame(summary(lmm_model_resolution)$coefficients)
+
+interval_df <- data.frame(term = models, i.Est = interval$Estimate,
+                          i.SE = interval$"Std", i.p.value = interval$"Pr")
+
+steps_df <- data.frame(s.Est = steps$Estimate,
+                       s.SE = steps$"Std", s.p.value = steps$"Pr")
+
+resolution_df <- data.frame(r.Est = resolution$Estimate,
+                            r.SE = resolution$"Std", r.p.value = resolution$"Pr")
+
+lmm_result_table <- cbind(interval_df, steps_df, resolution_df)
+
+write.csv(lmm_result_table, file = "./results_paleoTS_v0.6.1/table_lmm_uni.pdf", row.names = FALSE)
+
+
+
 #-------------------
 # PLOT ABSOLUTE FIT
 #-------------------
@@ -250,31 +297,36 @@ dev.off()
 plot_data2 <- plot_data 
 plot_data2 <- plot_data2 %>% drop_na(model_adequate)
 
-# micro vs. macro
-micro_macro <- plot_data2[c("model_adequate", "microfossil")]
+# ordering the model according to the number of parameters
 level_order <- c("stasis", "URW", "GRW", "accel", "decel", "OU", "OU mov opt")
-pdf("./results_paleoTS_v0.6.1/plot/micro_macro_adeq.pdf")
-ggplot(micro_macro, aes(x = factor(model_adequate, levels = level_order), fill = microfossil)) + geom_bar() +
-  theme_classic() + scale_x_discrete(labels = c("Stasis", "URW", "GRW", "Accel.", "Decel.", "OU", "OU mov. opt.")) +
-  labs(fill = "") + scale_fill_discrete(name = "", labels = c("Macrofossils", "Microfossils"), palette = col_val1) +
-  xlab("Model") + ylab("Count") + theme(legend.text = element_text(size = 15))
-dev.off()
+plot_data2$model_aicc <- factor(plot_data2$model_aicc, levels = level_order)
+plot_data2$model_aicc <- relevel(plot_data2$model_aicc, ref = "stasis")
+
+# micro vs. macro
+#micro_macro <- plot_data2[c("model_adequate", "microfossil")]
+#level_order <- c("stasis", "URW", "GRW", "accel", "decel", "OU", "OU mov opt")
+#pdf("./results_paleoTS_v0.6.1/plot/micro_macro_adeq.pdf")
+#ggplot(micro_macro, aes(x = factor(model_adequate, levels = level_order), fill = microfossil)) + geom_bar() +
+#  theme_classic() + scale_x_discrete(labels = c("Stasis", "URW", "GRW", "Accel.", "Decel.", "OU", "OU mov. opt.")) +
+#  labs(fill = "") + scale_fill_discrete(name = "", labels = c("Macrofossils", "Microfossils"), palette = col_val1) +
+#  xlab("Model") + ylab("Count") + theme(legend.text = element_text(size = 15))
+#dev.off()
 
 # environment
-env <- plot_data2[c("model_adequate", "environment")]
-level_order <- c("stasis", "URW", "GRW", "accel", "decel", "OU", "OU mov opt")
-pdf("./results_paleoTS_v0.6.1/plot/environment_adeq.pdf")
-ggplot(env, aes(x = factor(model_adequate, levels = level_order), fill = environment)) + geom_bar() +
-  theme_classic() + 
-  scale_x_discrete(labels = c("Stasis", "URW", "GRW", "Accel.", "Decel.", "OU", "OU mov. opt.")) +
-  labs(fill = "") + scale_fill_discrete(name = "Environment", labels = c("Lacustrine", "Marine",
-                                                                         "Terrestrial"), palette = col_val2[2:4]) +
-  xlab("Model") + ylab("Count") + theme(legend.text = element_text(size = 10), 
-                                        legend.title = element_text(size = 12),
-                                        axis.title = element_text(size = 12))
- dev.off()
+#env <- plot_data2[c("model_adequate", "environment")]
+#level_order <- c("stasis", "URW", "GRW", "accel", "decel", "OU", "OU mov opt")
+#pdf("./results_paleoTS_v0.6.1/plot/environment_adeq.pdf")
+#ggplot(env, aes(x = factor(model_adequate, levels = level_order), fill = environment)) + geom_bar() +
+#  theme_classic() + 
+#  scale_x_discrete(labels = c("Stasis", "URW", "GRW", "Accel.", "Decel.", "OU", "OU mov. opt.")) +
+#  labs(fill = "") + scale_fill_discrete(name = "Environment", labels = c("Lacustrine", "Marine",
+#                                                                         "Terrestrial"), palette = col_val2[2:4]) +
+#  xlab("Model") + ylab("Count") + theme(legend.text = element_text(size = 10), 
+#                                        legend.title = element_text(size = 12),
+#                                        axis.title = element_text(size = 12))
+# dev.off()
 
-# interval MY
+###### interval MY ######
 intv_my <- plot_data2[c("model_adequate", "interval_MY", "parameters")]
 
 
@@ -290,6 +342,11 @@ sink(file = "./results_paleoTS_v0.6.1/interval_my_results_adeq.txt")
 intv_my_mean
 intv_my_median
 sink()
+
+# LMM
+#lmm_model_time2 <- lmer(interval_MY ~ 1+ model_aicc + (1| popID), data = plot_data2)
+lmm_model_time2 <- lm(interval_MY ~ model_aicc, data = plot_data2)
+summary(lmm_model_time2)
 
 # plot
 level_order <- c("stasis", "URW", "accel", "decel", "OU", "OU mov opt")
@@ -307,7 +364,7 @@ intv_plot <- ggplot(intv_my, aes(x = interval_MY, y = factor(model_adequate, lev
 intv_plot
 dev.off()
 
-# steps
+###### steps ######
 steps <- plot_data2[c("model_adequate", "steps", "parameters")]
 
 # stats
@@ -322,6 +379,11 @@ sink(file = "./results_paleoTS_v0.6.1/steps_uni_results_adeq.txt")
 steps_mean
 steps_median
 sink()
+
+# LMM
+#lmm_model_steps2 <- lmer(steps ~ model_aicc + (1| popID), data = plot_data2)
+lmm_model_steps2 <- lm(steps ~ model_aicc, data = plot_data2)
+summary(lmm_model_steps2)
 
 # plot
 level_order <- c("stasis", "URW", "accel", "decel", "OU", "OU mov opt")
@@ -338,7 +400,7 @@ steps_plot <- ggplot(steps, aes(x = log(steps), y = factor(model_adequate, level
 steps_plot
 dev.off()
 
-# resolution
+###### resolution ######
 res <- plot_data2[c("model_adequate", "steps", "interval_MY", "parameters")]
 res$resolution <- res$steps/res$interval_MY
 
@@ -354,6 +416,12 @@ sink(file = "./results_paleoTS_v0.6.1/resolution_uni_results_adeq.txt")
 res_mean
 res_median
 sink()
+
+# LMM
+plot_data2$resolution = plot_data2$steps/plot_data2$interval_MY
+#lmm_model_resolution2 <- lmer(resolution ~ 1+ model_aicc + (1| popID), data = plot_data2)
+lmm_model_resolution2 <- lm(resolution ~ model_aicc, data = plot_data2)
+summary(lmm_model_resolution2)
 
 # plot
 level_order <- c("stasis", "URW", "accel", "decel", "OU", "OU mov opt")
@@ -378,3 +446,22 @@ grid.arrange(intv_plot,
              steps_plot,
              res_plot, nrow = 1)
 dev.off()
+
+# put the output of the linear mixed effects models in the same table
+models2 = rownames(summary(lmm_model_time2)$coefficients)
+interval2 = as.data.frame(summary(lmm_model_time2)$coefficients)
+steps2 = as.data.frame(summary(lmm_model_steps2)$coefficients)
+resolution2 = as.data.frame(summary(lmm_model_resolution2)$coefficients)
+
+interval_df2 <- data.frame(term = models2, i.Est = interval2$Estimate,
+                          i.SE = interval2$"Std", i.p.value = interval2$"Pr")
+
+steps_df2 <- data.frame(s.Est = steps2$Estimate,
+                       s.SE = steps2$"Std", s.p.value = steps2$"Pr")
+
+resolution_df2 <- data.frame(r.Est = resolution2$Estimate,
+                            r.SE = resolution2$"Std", r.p.value = resolution2$"Pr")
+
+lmm_result_table2 <- cbind(interval_df2, steps_df2, resolution_df2)
+
+write.csv(lmm_result_table2, file = "./results_paleoTS_v0.6.1/table_lmm_uni_adeq.pdf", row.names = FALSE)
